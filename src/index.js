e287c108af404de19e64d3bc8020070a77f15d9b
andysush <sushkoandy1@gmail.com>
2025-02-10 14:49:19 +0200

    fix pathes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import * as THREE from "three";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.164.0/build/three.module.min.js";
+import { OrbitControls } from "https://cdn.jsdelivr.net/npm/three@0.164.0/examples/jsm/controls/OrbitControls.js";
 import texturePath from "./public/logo-min.svg";
 
 const scene = new THREE.Scene();
---
+++ vite.config.js
@@ -0,0 +1,5 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+	base: "/anim-v3-/", // 🔹 Замініть на назву вашого GitHub репозиторію!
+});
